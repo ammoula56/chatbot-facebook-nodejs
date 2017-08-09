722776a2ae5e8f9c2e109c1949933498ecb6ae21
amal <ammoula56@hotmail.com>
2017-08-09 20:54:57 +0100

    jhbjh

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
+    FB_PAGE_TOKEN: 'EAAbo7PpGph4BAG1V6Ui9LzZAZB2MsIR9V9ZBPhPhdPhy4F5SCstq4obnBc1EDBcBgkYbsDjJIsvVEgBwfbCpuiDxRuZCqVSZAKIzeRF4dQXxzpwPcSgeCUlljSmakMPnJQv3wykjVjN1BklAdeaZA9q1x1ol0g0VNhfwt6kxIMn2pd16ZAo6JpQ',
     FB_VERIFY_TOKEN: '',
     API_AI_CLIENT_ACCESS_TOKEN: '',
     FB_APP_SECRET: '',
-    SERVER_URL: "",
+    SERVER_URL: "https://enigmatic-earth-55474.herokuapp.com/",
 };
